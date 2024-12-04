790d9830daf08f713800b1fbfdd34431caeac689
c-alsop <109773190+c-alsop@users.noreply.github.com>
2024-12-04 19:59:27 +1000

    Added comment in shared.h

--- shared.h
+++ shared.h
@@ -2,6 +2,10 @@
 #ifndef SHARED_H 
 #define SHARED_H
 
+/* get_contents_of_file()
+ * ~~~~~~~~~~~~~~~~~~~~~~
+ * Returns the contents of the file at file_path as a string.
+ */
 char* get_contents_of_file(char* file_path);
 
 #endif /* SHARED_H */
