d5f82d2d1192f42d7663a1a28ee01eee81ba20a8
c-alsop <109773190+c-alsop@users.noreply.github.com>
2024-12-05 12:24:18 +1000

    Added comments and minor function name change.

--- 2/2.h
+++ 2/2.h
@@ -10,10 +10,26 @@ typedef enum {
     DESCENDING
 } ORDER;
 
+/* check_levels()
+ * ~~~~~~~~~~~~~~
+ * Checks if the levels are safe for part 1. If they are unsafe set the error
+ * flag. In order to be safe, the order of the levels must be the same 
+ * (ascending or descending) and the difference between levels must be between 1 and 3.
+ */
 void check_levels(int levels_count, int* levels, int* order, int* error);
 
+/* check_levels()
+ * ~~~~~~~~~~~~~~
+ * Checks if the levels are safe for part 2. Safety conditions are the same
+ * as in part 1, but a single bad level can be tolerated.
+ */
 void check_levels_2(int levels_count, int* levels, int* order, int* error);
 
-void remove_elem(int** new_levels, int* levels, int levels_count, int index_to_remove);
+/* remove_level()
+ * ~~~~~~~~~~~~~
+ * Modifies new_levels so it contains levels but without whichever level is
+ * at the index_to_remove 
+ */
+void remove_level(int** new_levels, int* levels, int levels_count, int index_to_remove);
 
 #endif /* TASK_2_H */
